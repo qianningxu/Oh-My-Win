8819e0fc852078596a127abf7d0ba08d6744d892
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 19:20:51 +0100

    Use white background tint with darker focused tab

--- Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
+++ Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
@@ -2,8 +2,8 @@ import AppKit
 
 extension GeistColorTokens {
     // Neutral sRGB tints with darker focused fills for translucent compositing.
-    static let windowTab100Dark = NSColor(srgbRed: 253.0 / 255, green: 253.0 / 255, blue: 253.0 / 255, alpha: 1)
-    static let windowTab100Light = NSColor(srgbRed: 253.0 / 255, green: 253.0 / 255, blue: 253.0 / 255, alpha: 1)
+    static let windowTab100Dark = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
+    static let windowTab100Light = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
     static let windowTab200Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
     static let windowTab200Light = NSColor(srgbRed: 160.0 / 255, green: 160.0 / 255, blue: 160.0 / 255, alpha: 1)
     static let windowTab300Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
