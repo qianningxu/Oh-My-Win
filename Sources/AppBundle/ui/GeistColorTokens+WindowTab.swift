198f77bd25e3c166fe61a73fb97a4819a6d20d7a
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 19:28:32 +0100

    Further darken focused tab fill

--- Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
+++ Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
@@ -4,8 +4,8 @@ extension GeistColorTokens {
     // Neutral sRGB tints with darker focused fills for translucent compositing.
     static let windowTab100Dark = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
     static let windowTab100Light = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
-    static let windowTab200Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
-    static let windowTab200Light = NSColor(srgbRed: 160.0 / 255, green: 160.0 / 255, blue: 160.0 / 255, alpha: 1)
+    static let windowTab200Dark = NSColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+    static let windowTab200Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
     static let windowTab300Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
     static let windowTab300Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
 }
