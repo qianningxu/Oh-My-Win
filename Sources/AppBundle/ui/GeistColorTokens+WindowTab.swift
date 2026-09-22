25e5be2855cb9edf7f2b5c7ff30dc8e54c5df17d
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 19:39:22 +0100

    Use dark gray bar and lighter focused fill in dark mode

--- Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
+++ Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
@@ -1,10 +1,10 @@
 import AppKit
 
 extension GeistColorTokens {
-    // Neutral sRGB tints with darker focused fills for translucent compositing.
-    static let windowTab100Dark = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
+    // Neutral sRGB tints for the workspace tab bar.
+    static let windowTab100Dark = NSColor(srgbRed: 48.0 / 255, green: 48.0 / 255, blue: 48.0 / 255, alpha: 1)
     static let windowTab100Light = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
-    static let windowTab200Dark = NSColor(srgbRed: 0, green: 0, blue: 0, alpha: 1)
+    static let windowTab200Dark = NSColor(srgbRed: 160.0 / 255, green: 160.0 / 255, blue: 160.0 / 255, alpha: 1)
     static let windowTab200Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
     static let windowTab300Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
     static let windowTab300Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
