9d1871f19b19b3eec0efff66133d4626d2f9c4df
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 18:52:08 +0100

    Match sampled FDFDFD background and lighten focused fills

--- Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
+++ Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
@@ -2,10 +2,10 @@ import AppKit
 
 extension GeistColorTokens {
     // Neutral sRGB tints with darker focused fills for translucent compositing.
-    static let windowTab100Dark = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
-    static let windowTab100Light = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
-    static let windowTab200Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
-    static let windowTab200Light = NSColor(srgbRed: 160.0 / 255, green: 160.0 / 255, blue: 160.0 / 255, alpha: 1)
+    static let windowTab100Dark = NSColor(srgbRed: 253.0 / 255, green: 253.0 / 255, blue: 253.0 / 255, alpha: 1)
+    static let windowTab100Light = NSColor(srgbRed: 253.0 / 255, green: 253.0 / 255, blue: 253.0 / 255, alpha: 1)
+    static let windowTab200Dark = NSColor(srgbRed: 48.0 / 255, green: 48.0 / 255, blue: 48.0 / 255, alpha: 1)
+    static let windowTab200Light = NSColor(srgbRed: 208.0 / 255, green: 208.0 / 255, blue: 208.0 / 255, alpha: 1)
     static let windowTab300Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
     static let windowTab300Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
 }
