708889812b8f67298fc291849f8a906a32cc5c02
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 16:02:19 +0100

    Use neutral translucent tint so backdrop supplies tab color

--- Sources/AppBundle/ui/WindowTabCustomSurface.swift
+++ Sources/AppBundle/ui/WindowTabCustomSurface.swift
@@ -9,15 +9,10 @@ struct WindowTabCustomSurface: View {
             if !reduceTransparency {
                 VisualEffectBlur(material: .underWindowBackground, blendingMode: .behindWindow)
             }
-            LinearGradient(
-                colors: [
-                    Color(nsColor: palette.isDark ? GeistColorTokens.windowTab100Dark : GeistColorTokens.windowTab100Light),
-                    Color(nsColor: palette.isDark ? GeistColorTokens.windowTab200Dark : GeistColorTokens.windowTab200Light),
-                ],
-                startPoint: .top,
-                endPoint: .bottom
-            )
-            .opacity(reduceTransparency ? 1 : 0.94)
+            Color(nsColor: palette.isDark
+                ? GeistColorTokens.gray500Dark.nsColor
+                : GeistColorTokens.background1Light.nsColor)
+                .opacity(reduceTransparency ? 1 : (palette.isDark ? 0.72 : 0.60))
         }
         .clipShape(Capsule(style: .continuous))
     }
