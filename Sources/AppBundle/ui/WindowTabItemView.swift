17c932f3daedcd32837a42078e65567382360e8a
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 19:07:35 +0100

    Set background and focused fills to 25 percent transparency

--- Sources/AppBundle/ui/WindowTabItemView.swift
+++ Sources/AppBundle/ui/WindowTabItemView.swift
@@ -34,7 +34,7 @@ struct WindowTabItemView: View {
                 .fill(Color(nsColor: palette.isDark
                     ? GeistColorTokens.windowTab200Dark
                     : GeistColorTokens.windowTab200Light))
-                .opacity(tab.isActive ? 0.50 : 0)
+                .opacity(tab.isActive ? 0.75 : 0)
         }
         .contentShape(Rectangle())
     }
