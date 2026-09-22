a0c855f49d20b3508c674472181736159318cc74
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 19:57:34 +0100

    Use light gray border for dark workspace tab bar

--- Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
+++ Sources/AppBundle/ui/GeistColorTokens+WindowTab.swift
@@ -6,6 +6,6 @@ extension GeistColorTokens {
     static let windowTab100Light = NSColor(srgbRed: 1, green: 1, blue: 1, alpha: 1)
     static let windowTab200Dark = NSColor(srgbRed: 160.0 / 255, green: 160.0 / 255, blue: 160.0 / 255, alpha: 1)
     static let windowTab200Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
-    static let windowTab300Dark = NSColor(srgbRed: 16.0 / 255, green: 16.0 / 255, blue: 16.0 / 255, alpha: 1)
+    static let windowTab300Dark = NSColor(srgbRed: 208.0 / 255, green: 208.0 / 255, blue: 208.0 / 255, alpha: 1)
     static let windowTab300Light = NSColor(srgbRed: 96.0 / 255, green: 96.0 / 255, blue: 96.0 / 255, alpha: 1)
 }
