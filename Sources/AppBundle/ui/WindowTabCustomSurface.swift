99f4eadcd69573c97c1b5aae03dabcb3fe1be960
qianningxu <qx20@st-andrews.ac.uk>
2026-09-22 16:11:07 +0100

    Match bar transparency at forty percent in both modes

--- Sources/AppBundle/ui/WindowTabCustomSurface.swift
+++ Sources/AppBundle/ui/WindowTabCustomSurface.swift
@@ -12,7 +12,7 @@ struct WindowTabCustomSurface: View {
             Color(nsColor: palette.isDark
                 ? GeistColorTokens.windowTab100Dark
                 : GeistColorTokens.windowTab100Light)
-                .opacity(reduceTransparency ? 1 : (palette.isDark ? 0.40 : 0.60))
+                .opacity(reduceTransparency ? 1 : 0.60)
         }
         .clipShape(Capsule(style: .continuous))
     }
